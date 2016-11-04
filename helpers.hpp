1fa1e48013df8f835349c806475c4228f0dc8da2
pedrotp <pedro@pedrotp.com>
2016-11-04 10:41:28 -0400

    added generic helper files

---
+++ helpers.hpp
@@ -0,0 +1,6 @@
+
+#include <iostream>
+#include <limits> // std::numeric_limits
+
+int getInt(int minSize = std::numeric_limits<int>::min(), int maxSize = std::numeric_limits<int>::max()); // get, validate and return int input
+bool getYesNo(); // get and validate yes/no input; returns true (yes) or false (no)
