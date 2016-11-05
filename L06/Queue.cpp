dbad450dc562d17a4d3df8ed145811841fdaa88e
pedrotp <pedro@pedrotp.com>
2016-11-05 02:46:19 -0400

    L06 added basic scaffolding for the queue ds

---
+++ L06/Queue.cpp
@@ -0,0 +1,15 @@
+/*********************************************************************
+
+** Program Filename: Queue.cpp
+
+** Author: Pedro Torres-Picon
+
+** Date: 11/07/2016
+
+** Description: Lab 6 - This is the implementation file for the Queue
+class, which includes the source code for all the functions in the class.
+A Queue is a first in first out (FIFO) data structure.
+
+*********************************************************************/
+
+#include "Queue.hpp"
---
+++ L06/Queue.hpp
@@ -0,0 +1,31 @@
+/*********************************************************************
+
+** Program Filename: Queue.hpp
+
+** Author: Pedro Torres-Picon
+
+** Date: 11/07/2016
+
+** Description: Lab 6 - This is the interface file for a Queue class,
+which includes the class, variable and function declarations. A Queue
+is a first in first out (FIFO) data structure.
+
+*********************************************************************/
+
+#ifndef QUEUE_HPP
+#define QUEUE_HPP
+
+// Defines the interface of the Queue class
+class Queue
+{
+
+  private:
+
+
+  public:
+    Queue();
+    ~Queue() {};
+
+};
+
+#endif
