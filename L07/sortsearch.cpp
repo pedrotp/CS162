2f6cf37c22c71f36c109a8ab20a2d034859e6508
pedrotp <pedro@pedrotp.com>
2016-11-13 19:42:06 -0500

    L07 complete

--- L07/sortsearch.cpp
+++ L07/sortsearch.cpp
@@ -1,7 +1,37 @@
+/*********************************************************************
+
+** Program Filename: sortsearch.cpp
+
+** Author: Pedro Torres-Picon
+
+** Date: 11/14/2016
+
+** Description: Lab 7 - A program that showcases various sort and search
+functions. This is the implementation file, which includes the source
+code for all the functions.
+
+*********************************************************************/
 
 #include "sortsearch.hpp"
 
-// Linear Search, Gaddis page 595
+/*********************************************************************
+
+** Function: linearSearch()
+
+** Description: a linear search function based on pseudocode found on
+page 595 of the Gaddis textbook
+
+** Parameters: an array pointer, the integer size, the integer we are
+going to search for
+
+** Pre-Conditions: none
+
+** Post-Conditions: none
+
+** Return: the int index of the search term if found, -1 if not
+
+*********************************************************************/
+
 int linearSearch (int *array, int size, int target) {
   int pos = -1;
   int index = 0;
@@ -14,7 +44,25 @@ int linearSearch (int *array, int size, int target) {
   return pos;
 };
 
-// Bubble Sort, Gaddis page 605
+
+/*********************************************************************
+
+** Function: bubbleSort()
+
+** Description: a bubble sort function based on pseudocode found on
+page 605 of the Gaddis textbook
+
+** Parameters: an array pointer, and an integer that represents the
+size of the array
+
+** Pre-Conditions: all values in the array should be integers
+
+** Post-Conditions: none
+
+** Return: nothing
+
+*********************************************************************/
+
 void bubbleSort (int *array, int size) {
   bool swap;
   do {
@@ -30,13 +78,30 @@ void bubbleSort (int *array, int size) {
   } while (swap);
 };
 
-// Binary Search, Gaddis page 598
+/*********************************************************************
+
+** Function: binarySearch()
+
+** Description: a binary search function based on pseudocode found on
+page 598 of the Gaddis textbook
+
+** Parameters: an array pointer, the integer size, the integer we are
+going to search for
+
+** Pre-Conditions: none
+
+** Post-Conditions: none
+
+** Return: the int index of the search term if found, -1 if not
+
+*********************************************************************/
+
 int binarySearch (int *array, int size, int target) {
   int first = 0;
   int last = size - 1;
   int middle;
   int pos = -1;
-  while (pos == -1 && first <= last) { // [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  while (pos == -1 && first <= last) {
     middle = (last + first) / 2;
     if (array[middle] == target) {
       pos = middle;
@@ -49,13 +114,52 @@ int binarySearch (int *array, int size, int target) {
   return pos;
 };
 
+/*********************************************************************
+
+** Description: printArr()
+
+** Takes an array pointer and an integer size for the array, and
+prints the array to the screen in a human readable format
+
+** Parameters: a pointer to an array, and the size of the array as an
+int value
+
+** Pre-Conditions: none
+
+** Post-Conditions: none
+
+** Return: none
+
+*********************************************************************/
+
 void printArr (int *array, int size) {
   std::cout << "[";
   for (int i = 0; i < size - 1; i++) {
     std::cout << array[i] << ", ";
   }
-  std::cout << array[size - 1]<< "]";
+  if (size > 0) std::cout << array[size - 1];
+  std::cout << "]";
 };
+
+/*********************************************************************
+
+** Function: getInt()
+
+** Description: Gets and validates integer input from the user, with
+the option of entering a valid range for the int. If no minimum or
+maximum values are entered, they are set to the min and max allowed
+for integers
+
+** Parameters: minSize and maxSize, both ints with defaults
+
+** Pre-Conditions: Show the user instructions and ask them to enter
+an integer value
+
+** Post-Conditions: none
+
+** Return: the integer entered by the user
+
+*********************************************************************/
 
 int getInt(int minSize, int maxSize) {
 
--- L07/sortsearch.hpp
+++ L07/sortsearch.hpp
@@ -1,15 +1,29 @@
+/*********************************************************************
+
+** Program Filename: sortsearch.hpp
+
+** Author: Pedro Torres-Picon
+
+** Date: 11/14/2016
+
+** Description: Lab 7 - A program that showcases various sort and search
+functions. This is the interface file, which includes the function
+declarations.
+
+*********************************************************************/
 
 #ifndef SORTSEARCH_HPP
 #define SORTSEARCH_HPP
 
 #include <iostream>
-#include <ctime> // std::time
 #include <limits> // std::numeric_limits
 
 int linearSearch (int *array, int size, int target);
 void bubbleSort (int *array, int size);
 int binarySearch (int *array, int size, int target);
-void printArr (int *array, int size);
+
+// Helpers:
+void printArr (int *array, int size); // print an array to the screen
 int getInt(int minSize = std::numeric_limits<int>::min(), int maxSize = std::numeric_limits<int>::max()); // get, validate and return int input
 
 #endif
