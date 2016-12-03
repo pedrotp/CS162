9e2bfd88d45ab808df8ec23a619db16fcc8c7843
pedrotp <pedro@pedrotp.com>
2016-12-03 09:42:48 -0500

    added destructor and constructor to space class

---
+++ CS162/FP/Space.cpp
@@ -0,0 +1,24 @@
+/*********************************************************************
+
+** Program Filename: Space.hpp
+
+** Author: Pedro Torres-Picon
+
+** Date: 12/07/2016
+
+** Description: Final Project - Part of a single-player game depicting
+the challenge of navigating the DMV. This is the implementation file
+for the Space abstract base class, which includes the source code for
+all the functions in the class.
+
+*********************************************************************/
+
+#include "Space.hpp"
+
+Space::Space() {
+
+};
+
+Space::~Space() {
+
+};
--- CS162/FP/Space.hpp
+++ CS162/FP/Space.hpp
@@ -6,38 +6,25 @@
 
 ** Date: 12/07/2016
 
-** Description: Final Project - Part of a single-player game involving
-a .
-This is the interface file for the Space abstract base class,
-which includes some of the class, variable and function declarations.
+** Description: Final Project - Part of a single-player game depicting
+the challenge of navigating the DMV. This is the interface file for
+the Space abstract base class, which includes some of the class,
+variable and function declarations.
 
 *********************************************************************/
 
 #ifndef SPACE_HPP
 #define SPACE_HPP
 
-#include <cstdlib> // rand()
-#include <ctime> // std::time
-#include <string>
-
 // Defines the interface of the Space class
 class Space
 {
   protected:
-    int attackDice[2]; // pos 0 represents the number of dice, and pos 1 the number of sides
-    int defenseDice[2]; // pos 0 represents the number of dice, and pos 1 the number of sides
-    int armor;
-    int maxStrength;
-    int strength;
-    std::string cName;
+
 
   public:
-    Space(int attackNum, int attackSides, int defenseNum, int defenseSides, int armorPts, int strengthPts, std::string spaceName);
-    std::string name(); // getter for cName
-    void setName(std::string spaceName); // setter for cName
-    virtual int attack();
-    virtual bool defend(int attackRes);
-    int recover();
+    Space();
+    ~Space();
 };
 
 #endif
